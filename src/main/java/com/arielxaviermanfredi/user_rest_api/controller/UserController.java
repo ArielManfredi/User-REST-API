c46b7c1a002a86cdbbe4430c28556c305acdd654
Ariel Xavier Manfredi <ariel.xavier.manfredi@gmail.com>
2024-09-27 16:51:23 -0300

    PUT Method on "/user" verifications changed.

--- src/main/java/com/arielxaviermanfredi/user_rest_api/controller/UserController.java
+++ src/main/java/com/arielxaviermanfredi/user_rest_api/controller/UserController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.arielxaviermanfredi.user_rest_api.model.User;
 import com.arielxaviermanfredi.user_rest_api.service.UserService;
 
-
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -24,79 +23,93 @@ public class UserController {
     @Autowired
     UserService service;
 
-
-
-
     @PostMapping
     public String addUser(@RequestBody User user) {
         User existentUser = service.getUserFull(user.getName(), user.getEmail(), user.getPassword());
 
-        if (existentUser!=null) {
+        if (existentUser != null) {
             return "User already registered!";
         }
 
-        if(user.getPassword()==null){
+        if (user.getPassword() == null) {
             return "Can not save the user without a password!";
+        } else if (user.getEmail() == null && user.getName() == null) {
+            return "Name or Email is needed!";
         }
 
         User savedUser = service.saveUser(user);
 
         if (savedUser != null) {
-            return "User "+savedUser.getName()+" saved successfully!\n" + savedUser;
+            return "User " + savedUser.getName() + " saved successfully!\n" + savedUser;
         }
-        return "Could not save the user "+user.getName();
+        return "Could not save the user " + user.getName();
     }
-
 
     @GetMapping
     public ResponseEntity<?> getUser(String name, String email) {
-        if (name!=null || email!=null) {
+        if (name != null || email != null) {
             User foundUser = service.getUser(name, email);
-            
-            if (foundUser!=null) {
+
+            if (foundUser != null) {
                 return ResponseEntity.ok(foundUser);
             }
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Could not find the User by the name "+name+" or the email "+email);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Could not find the User by the name " + name + " or the email " + email);
         }
-        return ResponseEntity.badRequest().body("To get an User, put an Name or Email in a Path Variable, like: http://localhost:8080/user?name=Ariel or http://localhost:8080/user?email=ariel.xavier.manfredi@gmail.com");
+        return ResponseEntity.badRequest().body(
+                "To get an User, put an Name or Email in a Path Variable, like: http://localhost:8080/user?name=Ariel or http://localhost:8080/user?email=ariel.xavier.manfredi@gmail.com");
     }
-
 
     @PutMapping
     public String editUser(@RequestBody User user) {
-        User existentUser = service.getUserFull(user.getName(), user.getEmail(), user.getPassword());
 
-        if (existentUser!=null) { return "User credentials is identical the registered one."; }
+        if (user.getId() == null) {
+            return "Can't edit the user without an ID.";
+        } else if (user.getPassword() == null) {
+            return "Can't save the user without a password.";
+        } else if (user.getEmail() == null && user.getName() == null) {
+            return "Name or Email is needed!";
+        }
+        try {
 
-        if(user.getPassword()==null){ return "Can't save the user without a password."; }
+            User userGotById = service.getUserById(user.getId());
 
-        if (user.getId()==null) { return "Can't edit the user without an ID."; }
-
-        User getByIdUser = service.getUserById(user.getId());
-
-        if (getByIdUser!=null) {
-            getByIdUser.setName(user.getName()!=getByIdUser.getName() ? user.getName() : getByIdUser.getName());
-            getByIdUser.setEmail(user.getEmail()!=getByIdUser.getEmail() ? user.getEmail() : getByIdUser.getEmail());
-            getByIdUser.setPassword(user.getPassword()!=getByIdUser.getPassword() ? user.getPassword() : getByIdUser.getPassword());
-
-            User savedUser = service.saveUser(user);
-    
-            if (savedUser != null) {
-                return "User "+savedUser.getName()+" edited successfully!\n" + savedUser;
+            if (userGotById.getName().equals(user.getName())  &&
+                    userGotById.getEmail().equals(user.getEmail()) &&
+                    userGotById.getPassword().equals(user.getPassword())) {
+                return "User credentials is identical the registered one.";
             }
+
+            if (userGotById != null) {
+
+                userGotById.setName(user.getName() != userGotById.getName() ? user.getName() : userGotById.getName());
+                userGotById.setEmail(user.getEmail() != userGotById.getEmail() ? user.getEmail() : userGotById.getEmail());
+                userGotById.setPassword(user.getPassword() != userGotById.getPassword() ? user.getPassword() : userGotById.getPassword());
+
+                User savedUser = service.saveUser(userGotById);
+
+                if (savedUser != null) {
+                    return "User " + savedUser.getName() + " edited successfully!\n" + savedUser;
+                }
+            }
+        } catch (Exception e) {
+            return "Could not edit the user " + user.getName() + ", the Id is incorrect or some credential.";
         }
 
-        return "Could not edit the user "+user.getName()+". Provide the id and ";
+        return "Could not edit the user " + user.getName() + ", check your credentials and try again.";
     }
 
     @DeleteMapping
     public String deleteUser(UUID id) {
-        System.out.println("ID: "+id);
-        if (id==null) { return "Can't delete an user without an Id..."; }
+        System.out.println("ID: " + id);
+        if (id == null) {
+            return "Can't delete an user without an Id...";
+        }
 
         String deleteMessage = service.deleteUser(id);
 
-        return deleteMessage!=null ? deleteMessage : "Could not delete the user of Id \""+id +"\" or inexistent Id.";
+        return deleteMessage != null ? deleteMessage
+                : "Could not delete the user of Id \"" + id + "\" or inexistent Id.";
     }
-    
+
 }
